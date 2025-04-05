e5ee80e0873a5b74ee9ad1cabafb06df88de4cb6
francinezgrr <fnfzaguirre@gmail.com>
2025-04-05 10:31:46 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBbzsYeIL8dJl07wryypxElw7J-wwuJg80",
+  authDomain: "midterms-e7685.firebaseapp.com",
+  databaseURL: "https://midterms-e7685-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "midterms-e7685",
+  storageBucket: "midterms-e7685.firebasestorage.app",
+  messagingSenderId: "70295569245",
+  appId: "1:70295569245:web:436943e1cf37c11cf31eb4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
